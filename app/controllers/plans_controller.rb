a3cb7120824671c9312715cf8ba74c4f19d37a29
Vincent Fleischacker <vdynmx@gmail.com>
2014-11-26 06:07:49 +0000

    setup stripe

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,20 +1,40 @@
 class PlansController < ApplicationController
-  def new
-    @plans = Plan.new
+
+   def new
+   @stripe_btn_data = {
+     key: "#{ Rails.configuration.stripe[:publishable_key] }",
+     description: "BigMoney Membership - ",
+     amount: 10_00
+   }
   end
 
   def create
-    @wiki = current_user.wikis.build(params.require(:wiki).permit(:public == true))
-    authorize @wiki
-    if @wiki.save
-      flash[:notice] = "Wiki was saved."
-      redirect_to @wiki
-      # report success
-    else
-      flash[:error] = "There was an error saving your wiki. Please try again."
-      render :new
+ 
+   # Creates a Stripe Customer object, for associating
+   # with the charge
+   customer = Stripe::Customer.create(
+     email: current_user.email,
+     card: params[:stripeToken]
+   )
+ 
+   # Where the real magic happens
+   charge = Stripe::Charge.create(
+     customer: customer.id, # Note -- this is NOT the user_id in your app
+     amount: Amount.default,
+     description: "BigMoney Membership - #{current_user.email}",
+     currency: 'usd'
+   )
+ 
+   flash[:success] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
+   current_user
+   redirect_to user_path(current_user) # or wherever
+ 
+ # Stripe will send back CardErrors, with friendly messages
+ # when something goes wrong.
+ # This `rescue block` catches and displays those errors.
+ rescue Stripe::CardError => e
+   flash[:error] = e.message
+   redirect_to new_charge_path
+ end
 
-    #update the role model based on user selection
-  end
-end
 end
